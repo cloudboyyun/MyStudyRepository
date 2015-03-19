7bbeaf05f9248ce41103fb75eb94466c901408cf
cloudboyyun <yun.xia2006@hotmail.com>
2015-03-19 15:05:34 +0800

    updated

--- StudyPacketFormat/src/main/java/study/packetFormat/HessianTest.java
+++ StudyPacketFormat/src/main/java/study/packetFormat/HessianTest.java
@@ -1,19 +1,21 @@
 package study.packetFormat;
 
-import java.io.StringWriter;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 
 import org.apache.jmeter.config.Arguments;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerClient;
 import org.apache.jmeter.protocol.java.sampler.JavaSamplerContext;
 import org.apache.jmeter.samplers.SampleResult;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.codehaus.jackson.map.annotate.JsonSerialize.Inclusion;
+
+import com.caucho.hessian.io.Hessian2Input;
+import com.caucho.hessian.io.Hessian2Output;
 
 import study.packetFormat.model.DataFactory;
 import study.packetFormat.model.xml.Message;
 
-public class JacksonJsonTest implements JavaSamplerClient {
-	private static ObjectMapper mapper = null;
+public class HessianTest implements JavaSamplerClient {
 	
 	@Override
 	public Arguments getDefaultParameters() {
@@ -22,8 +24,6 @@ public class JacksonJsonTest implements JavaSamplerClient {
 	
 	@Override
 	public void setupTest(JavaSamplerContext arg0) {
-		mapper = new ObjectMapper();
-		mapper.setSerializationInclusion(Inclusion.NON_NULL);
 	}
 
 	@Override
@@ -31,11 +31,11 @@ public class JacksonJsonTest implements JavaSamplerClient {
 		SampleResult result = new SampleResult();
 		try {
 	        result.sampleStart();
+	        
 	        Message message = DataFactory.getMessage();
-			StringWriter stringWriter = new StringWriter();
-			mapper.writeValue(stringWriter, message);
-			String jsonResult = stringWriter.toString();
-			Message messageResult = mapper.readValue(jsonResult, Message.class);
+	        
+	        byte[] pbResult = toHessian(message);
+			Message messageResult = (Message)fromHessian(pbResult);
 			if(DataFactory.password.equals(messageResult.getMsgContent().getPassword())) {
 				result.setSuccessful(true);
 			} else {
@@ -47,6 +47,23 @@ public class JacksonJsonTest implements JavaSamplerClient {
 			result.setSuccessful(false);
 			e.printStackTrace();
 		}
+		return result;
+	}
+	
+	private byte[] toHessian(Object message) throws IOException {
+		ByteArrayOutputStream bos = new ByteArrayOutputStream();
+		Hessian2Output out = new Hessian2Output(bos);
+		out.writeObject(message);
+		out.flush();
+		bos.close();
+		return bos.toByteArray();
+	}
+	
+	protected Object fromHessian(byte[] hessianBytes) throws IOException {
+		ByteArrayInputStream bin = new ByteArrayInputStream(hessianBytes);
+		Hessian2Input in = new Hessian2Input(bin);
+		Object result = in.readObject();
+		bin.close();
 		return result;
 	}
 
