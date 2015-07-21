4ea65f49d2a0de36f42c46bd69de547b994438c8
cloudboyyun <yun.xia2006@hotmail.com>
2015-07-22 00:21:10 +0800

    added

--- StudyLog4j2/src/main/java/com/cloudboy/studyLog4j2/Log4j2Study.java
+++ StudyLog4j2/src/main/java/com/cloudboy/studyLog4j2/Log4j2Study.java
@@ -1,9 +1,10 @@
 package com.cloudboy.studyLog4j2;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Log4j2Study {
-	private static Logger logger = Logger.getLogger(Log4j2Study.class);
+	private static Logger logger = LogManager.getLogger(Log4j2Study.class);
 	
 	public static void main(String[] args) {
 		logger.info("Using log4j2.");
