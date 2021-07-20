0642c6ed0865956b452c7e5493f671dcb122e87f
xiayun <yun.xia2006@hotmail.com>
2021-07-20 22:40:14 +0800

    111

---
+++ StudyUniApp/StudyUniCloud/pages/t_news/detail.vue
@@ -0,0 +1,114 @@
+<template>
+  <view class="container">
+    <unicloud-db ref="udb" v-slot:default="{data, loading, error, options}" :options="options" collection="t_news" field="title,time,channel,pic,status" :where="queryWhere" :getone="true" :manual="true">
+      <view v-if="error">{{error.message}}</view>
+      <view v-else-if="loading">
+        <uni-load-more :contentText="loadMore" status="loading"></uni-load-more>
+      </view>
+      <view v-else-if="data">
+        <view>
+          <text>标题</text>
+          <text>{{data.title}}</text>
+        </view>
+        <view>
+          <text>时间</text>
+          <uni-dateformat :threshold="[0, 0]" :date="data.time"></uni-dateformat>
+        </view>
+        <view>
+          <text>频道</text>
+          <text>{{data.channel}}</text>
+        </view>
+        <view>
+          <text>图片</text>
+          <uni-link :href="data.pic" :download="data.pic" :text="data.pic"></uni-link>
+        </view>
+        <view>
+          <text>状态</text>
+          <uni-data-picker :localdata="options.status_valuetotext" :value="data.status" :multiple="false" :readonly="true" :arrow="false" split=","></uni-data-picker>
+        </view>
+      </view>
+    </unicloud-db>
+    <view class="btns">
+      <button type="primary" @click="handleUpdate">修改</button>
+      <button type="warn" class="btn-delete" @click="handleDelete">删除</button>
+    </view>
+  </view>
+</template>
+
+<script>
+  // 由schema2code生成，包含校验规则和enum静态数据
+  import { enumConverter } from '../../js_sdk/validator/t_news.js';
+
+  export default {
+    data() {
+      return {
+        queryWhere: '',
+        loadMore: {
+          contentdown: '',
+          contentrefresh: '',
+          contentnomore: ''
+        },
+        options: {
+          // 将scheme enum 属性静态数据中的value转成text
+          ...enumConverter
+        }
+      }
+    },
+    onLoad(e) {
+      this._id = e.id
+    },
+    onReady() {
+      if (this._id) {
+        this.queryWhere = '_id=="' + this._id + '"'
+      }
+    },
+    methods: {
+      handleUpdate() {
+        // 打开修改页面
+        uni.navigateTo({
+          url: './edit?id=' + this._id,
+          events: {
+            // 监听修改页面成功修改数据后, 刷新当前页面数据
+            refreshData: () => {
+              this.$refs.udb.loadData({
+                clear: true
+              })
+            }
+          }
+        })
+      },
+      handleDelete() {
+        this.$refs.udb.remove(this._id, {
+          success: (res) => {
+            // 删除数据成功后跳转到list页面
+            uni.navigateTo({
+              url: './list'
+            })
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style>
+  .container {
+    padding: 10px;
+  }
+
+  .btns {
+    margin-top: 10px;
+    /* #ifndef APP-NVUE */
+    display: flex;
+    /* #endif */
+    flex-direction: row;
+  }
+
+  .btns button {
+    flex: 1;
+  }
+
+  .btn-delete {
+    margin-left: 10px;
+  }
+</style>
